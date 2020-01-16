02ec31341532faab143e16b8458f350d73d7932d
Guven Degirmenci <duoquote@outlook.com>
2020-01-16 20:27:05 +0300

    A few updates.

--- index.js
+++ index.js
@@ -1,48 +1,60 @@
-var Discord = require('discord.js');
+const Discord = require('discord.js');
+const client = new Discord.Client();
+
 const fs = require('fs');
 const conf = require("./conf.js");
 const cmdLang  = require("./cmdLang.js");
 const MongoClient = require('mongodb').MongoClient;
 
-function recurseObj(obj, ind=null) {
-  var data = [];
-  for (var k in obj) {
-    if (k == "validate") {
-      continue;
-    }
-    if (typeof obj[k] == "object") {
-      recurseObj(obj[k], (ind) ? (`${ind}.${k}`):(k)).forEach((e)=>{
-        data.push(e)
-      })
-    } else {
-      data.push(`${(ind) ? (ind+"."+k):k}:${obj[k]}`)
-    }
-  }
-  return data;
-}
 
-Discord.Presence.prototype.validate = function(comp) {
-  obj = [
-    recurseObj(this),
-    recurseObj(comp)
-  ]
-  if (obj[0].length != obj[1].length) {
-    return false;
-  } else {
-    for (var i = 0; i < obj[0].length; i++) {
-      if (obj[0][i] != obj[1][i]) {
-        return false;
-      }
-    }
-    return true;
-  }
-}
+// // I used this function to compare two objects, was good, may reference later for
+// // other purposes. Needs some fix though, Arrays are not included in comparison.
+// function recurseObj(obj, ind=null) {
+//   var data = [];
+//   for (var k in obj) {
+//     if (k == "validate") {
+//       continue;
+//     }
+//     if (typeof obj[k] == "object") {
+//       recurseObj(obj[k], (ind) ? (`${ind}.${k}`):(k)).forEach((e)=>{
+//         data.push(e)
+//       })
+//     } else {
+//       data.push(`${(ind) ? (ind+"."+k):k}:${obj[k]}`)
+//     }
+//   }
+//   return data;
+// }
+//
+// Discord.Presence.prototype.validate = function(comp) {
+//   obj = [
+//     recurseObj(this),
+//     recurseObj(comp)
+//   ]
+//   if (obj[0].length != obj[1].length) {
+//     return false;
+//   } else {
+//     for (var i = 0; i < obj[0].length; i++) {
+//       if (obj[0][i] != obj[1][i]) {
+//         return false;
+//       }
+//     }
+//     return true;
+//   }
+// }
 
-const client = new Discord.Client();
 
 // Will add a config option for mongoURL variable.
 var mongoURL = "mongodb://localhost:27017/discord";
 var serverData = {}
+var dcToken;
+
+if (fs.existsSync("./dcToken.js")) {
+  dcToken = require('./dcToken.js');
+} else {
+  console.log("Put your token inside 'dcTokenExample.js' and rename it to 'dcToken.js'");
+  exit();
+}
 
 function reloadServerData(callback=null) {
   MongoClient.connect(mongoURL, (err, db)=>{
@@ -62,17 +74,6 @@ function reloadServerData(callback=null) {
   })
 }
 
-reloadServerData();
-
-var dcToken;
-
-if (fs.existsSync("./dcToken.js")) {
-  dcToken = require('./dcToken.js');
-} else {
-  console.log("Put your token inside 'dcTokenExample.js' and rename it to 'dcToken.js'");
-  exit();
-}
-
 
 function cmdParser(cmdIn, prefix) {
   return cmdIn.split(/ +/)
@@ -89,10 +90,11 @@ function cmdParser(cmdIn, prefix) {
     });
 }
 
-
+reloadServerData();
 
 client.on('ready', () => {
   console.log(`Bot "${client.user.tag}" is ready!`);
+  // console.log(client.guilds.get("393057072079568907").presences);
   //console.log(client);
 });
 
@@ -167,9 +169,6 @@ client.on('message', msg => {
       }
     }
 
-
-
-
     if (cmdDef.cmd == "setl") {
       if (cmdDef.params.length == 1) {
         if (cmdDef.params[0].toUpperCase() in cmdLang) {
@@ -193,58 +192,62 @@ client.on('message', msg => {
   }
 });
 
-// // Make the program clear last presence data after a while.
-// setInterval(()=>{
-//   for (var presence in presenceLast) {
-//     console.log(presenceLast);
-//     console.log(new Date() - presenceLast[presence].timestamp);
-//     if (new Date() - presenceLast[presence].timestamp > 5000) {
-//       delete presenceLast[presence];
-//       console.log(`Deleted last presence data for user ${presenceLast[presence].user}.`);
-//     }
-//   }
-// }, 3000)
-
-var presenceLast = {};
-
-// Added presence tracking feature.
-client.on("presenceUpdate", (oldPres, newPres)=> {
-  if (newPres.user.bot) return;
-  // Format:
-  // {
-  //   user: "user_id",
-  //   timestamp: 1578852296892,
-  //   presence: Object -> Discord.Presence data
-  // }
-  var pres = {
-    user: newPres.user.id,
-    timestamp: new Date(),
-    presence: newPres.guild.presences.get(newPres.user.id)
-  }
-  if (pres.user in presenceLast) {
-    if (!presenceLast[pres.user].validate(pres.presence)) {
-      insertPresence(pres);
-    }
-  } else {
-    insertPresence(pres);
-  }
-  presenceLast[pres.user] = pres.presence;
-})
-
-
-// Will move this function into another file to store all specific functions
-// in one place later.
-function insertPresence(pres) {
-  MongoClient.connect(mongoURL, function(err, db) {
-    if (err) throw err;
-    var dbo = db.db("discord");
-    dbo.collection("user_presence_history").insertOne(pres, (err, res)=>{
-      if (err) throw err;
-      // console.log(`Saved data for user ${upd.user.id}.`);
-      db.close();
-    });
-  });
-}
-
-
 client.login(dcToken.token);
+
+////////////////////////////////////////////////////////////////////////////////
+// // Presence tracking feature turned out to be SOOO buggy, so I am going to
+// // abandon it, at least for now.
+//
+//
+//
+// var presenceLast = [{}, {}];
+//
+// // Added presence tracking feature.
+// client.on("presenceUpdate", (oldPres, newPres)=> {
+//   if (newPres.user.bot) return;
+//   // Format:
+//   // {
+//   //   user: "user_id",
+//   //   timestamp: 1578852296892,
+//   //   presence: Object -> Discord.Presence data
+//   // }
+//   var pres = {
+//     user: newPres.user.id,
+//     timestamp: new Date(),
+//     presence: newPres.guild.presences.get(newPres.user.id)
+//   }
+//   console.log(pres.presence);
+//   console.log(oldPres.frozenPresence);
+//   console.log("-------------------------------------------------------------");
+//   if (pres.user in presenceLast[0]) {
+//     if (JSON.stringify(pres.presence) != JSON.stringify(presenceLast[0][pres.user])) {
+//       insertPresence(pres);
+//     }
+//     if (pres.user in presenceLast[1]) {
+//       if (JSON.stringify(oldPres.frozenPresence) != JSON.stringify(presenceLast[1][pres.user])) {
+//         pres.presence = oldPres.frozenPresence;
+//         insertPresence(pres);
+//       }
+//     }
+//     presenceLast[1][pres.user] = presenceLast[0][pres.user];
+//   } else {
+//     insertPresence(pres);
+//   }
+//   presenceLast[0][pres.user] = newPres.guild.presences.get(newPres.user.id);
+//
+// })
+//
+//
+// // Will move this function into another file to store all specific functions
+// // in one place later.
+// function insertPresence(pres) {
+//   MongoClient.connect(mongoURL, function(err, db) {
+//     if (err) throw err;
+//     var dbo = db.db("discord");
+//     dbo.collection("user_presence_history").insertOne(pres, (err, res)=>{
+//       if (err) throw err;
+//       // console.log(`Saved data for user ${upd.user.id}.`);
+//       db.close();
+//     });
+//   });
+// }
